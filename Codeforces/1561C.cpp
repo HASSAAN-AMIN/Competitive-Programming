dbd97c35c21ed6b2dfb02d9794028268b0aa84e7
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-08-22 05:51:21 +0500

    this was the third hottest thing i ever saw second was an another question and first ahm my toti ofc

--- Codeforces/1561C.cpp
+++ Codeforces/1561C.cpp
@@ -7,7 +7,20 @@
 #include<algorithm>
 
 using namespace std;
+bool check( int mid , vector<int> &req , vector<vector<int> >& arr){
 
+    int crnt = mid ;
+
+    for (int i = 0; i < arr.size(); i++) {
+        if( crnt >= req[i]){
+            crnt += arr[i].size() ;
+        }else{
+            return false;
+        }
+    }
+    return true;
+
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -18,13 +31,58 @@ int main() {
         int n;
         cin >> n;
 
-        vector<int> arr(n);
+        vector< vector<int> > arr(n );
 
         for (int i = 0; i < n; i++) {
-            cin >> arr[i];
+            int x  ;
+            cin >> x ;
+            while(x--){
+                int y ;
+                cin >> y;
+                arr[i].push_back(y); 
+            }
+        }
+        vector<int> req(n) ;
+
+        for (int i = 0; i < n; i++) {
+            int idx = 0 ;
+            int big = -1;
+            for (int j = 0; j < arr[i].size(); j++) {
+                if( arr[i][j] -j >=  big - idx  ){ // = imp very guess so 
+                    big = arr[i][j] ;
+                    idx = j ;
+                }
+            }
+            req[i] = big - idx + 1 ;
+            arr[i][0] = req[i]; 
         }
 
-        
+        sort(req.begin(), req.end());
+
+        sort(arr.begin(), arr.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        // easy lol
+
+        int l = req[0] ;
+        int r = req[n-1] ;
+
+        // 0000011111 type binary search
+        while(l < r){
+            int mid = l + (r-l)/2 ;
+
+            if( check( mid , req , arr )){
+                r =mid;
+            }else{
+                l = mid+ 1;
+            }
+
+        }   
+
+        cout << l << '\n';
+
+
+
     }
 
     return 0;
