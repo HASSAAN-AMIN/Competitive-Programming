6b9b6456c328c4d9d6898b4781279fb85a482f4b
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-08-28 21:15:29 +0500

    big 20 years (almost) of life spent on average i already have spent my 1 3rd part of life and after 40 the life is already dull so remains just 20 years in which most of the people have so many duties and stuff to make , life is a scam

--- Codeforces/1077C.cpp
+++ Codeforces/1077C.cpp
@@ -12,31 +12,40 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while(t--){
+    // int t;
+    // cin >> t;
+    // while(t--){
         int n;
         cin >> n;
 
         vector<int> arr(n);
 
         long long sum =  0;
+
+        int maxy = 1e6 ;
+        vector<int>  req( maxy + 1 , 0 );
         for (int i = 0; i < n; i++) {
             cin >> arr[i];
+            req[arr[i]]++;
             sum += arr[i]; 
         }
 
-        vector<int>  req(n);
-
-
-        for (int i = 0; i < n ; i++) {
-            if( 2*arr[i])
+        vector<int> ans ;
+        for (int i = 0; i < n; i++) {
+            sum -= arr[i]; 
+            req[arr[i]]--;
+            if( sum%2 == 0 && sum/2 <= 1e6 &&  req[sum/2]  )
+                ans.push_back(i+1) ;
+            req[arr[i]]++;
+            sum += arr[i];
+        }
+        cout << ans.size()  << '\n' ;
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << ' '  ;
         }
 
-
-
         
-    }
+   // }
 
     return 0;
-}
+} 
