2ef7706a6d6c55e9bfd5e00021f5ce0eecb49c1f
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-09-07 21:50:53 +0500

    yk i cant see deleted things when written and deleted in some time i can see only when written for long time  , for now i jus see smth written and removed idk what when why

--- Codeforces/1215B.cpp
+++ Codeforces/1215B.cpp
@@ -24,13 +24,64 @@ int main() {
             arr[i] = -1;
     }
 
+    // p p p p n n p
+    // p p p p n p p
+
+    // for (int i = 1; i < n; i++) {
+    //     arr[i] *=  arr[i-1];
+    // }  
+    // for product till here arr[i] tells
+    // cant check all n is large
+
+    // total n*n + n / 2
+
+    // even negs make pos
+
+    // maintain a cnt 
+    // of negs
+
+    // when negs become odd , add the combination sum till now
+    // maybe ?
+    // uhh 
+
+
+    // why think hard when i can just dp  ? 
+    vector<vector<int>> dp(n , vector<int>(2) );
+
+
+    // col 0 pos   ,  1 neg
+
+    // base
+    if( arr[0] >  0){
+        dp[0][0] = 1;
+        dp[0][1] = 0 ;
+    }else{
+        dp[0][0] = 0 ;
+        dp[0][1] = 1 ;
+    }
+
     for (int i = 1; i < n; i++) {
-        arr[i] *=  arr[i-1]
+        if(arr[i] > 0){
+            dp[i][0] = 1 + dp[i-1][0];
+            dp[i][1] = dp[i-1][1];
+        }else{
+            dp[i][1] = 1 + dp[i-1][0]; // cross cnt
+            dp[i][0] = dp[i-1][1];
+        }
     }
 
 
-    
+    long long pos = 0 ;
+    long long neg =  0;
 
+    
+    for (int i = 0; i < n; i++) {
+        pos += dp[i][0];
+        neg += dp[i][1];
+    }
+
+
+    cout << neg << ' ' << pos << '\n';
 
 
 
---
+++ Codeforces/982C.cpp
@@ -0,0 +1,60 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int dfs( int u , int prev , vector<vector<int>> &gr , int &cnt){
+
+    int cc= 0 ;
+    for( auto v : gr[u]){
+        if(v == prev)
+            continue;
+        cc  += dfs( v , u , gr,  cnt) ;
+    }
+    if( (1 + cc)%2 ){
+        return 1+cc ;
+    }else{
+        cnt++;
+        return 0 ;
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // int t;
+    // cin >> t;
+    // while(t--){
+        int n;
+        cin >> n;
+
+        if(n%2){
+            cout << -1 << '\n';
+            return 0 ;
+        }
+        vector< vector<int >> gr (n) ;
+
+
+        for (int i = 0; i < n- 1; i++) {
+            int x ,y ;
+            cin >> x >> y ;
+            x-- ; y--;
+            gr[x].push_back(y);
+            gr[y].push_back(x);
+        }
+        int cnt  = 0 ;
+        int x =  dfs( 0 , -1 , gr  ,cnt );
+
+        cout << cnt -1 << '\n';
+        
+  //  }
+
+    return 0;
+}
