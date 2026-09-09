f0fb7d5df94239cf7e2b91dcdb4f5c62027f4dd9
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-09-10 02:36:33 +0500

    i cant see , i am curious and you are angry , adapco gel also doesnt works its been 6 months

---
+++ Codeforces/2260D.cpp
@@ -0,0 +1,30 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+        
+        
+
+        
+    }
+
+    return 0;
+}
