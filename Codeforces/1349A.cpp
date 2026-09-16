3835e2b7bfb5dd46330dac5b7d3df7c77046f5e4
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-09-16 21:21:39 +0500

    hi saim

---
+++ Codeforces/1349A.cpp
@@ -0,0 +1,35 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        // gcd of lcms
+        // lcm  >= max a_i
+        // gcd_of_lcms <= lcm >= max( a_i)
+
+        
+    }
+
+    return 0;
+}
